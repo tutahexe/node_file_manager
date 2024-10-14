dfe60b483231e90bb1c1e1891319eefab1e7f3ca
Mike <againstthesilence@gmail.com>
2024-10-14 23:52:59 +0300

    reworked mv to use streams

--- src/move.js
+++ src/move.js
@@ -2,20 +2,25 @@ import fs from "node:fs";
 import path from "node:path";
 
 export const moveFile = async (oldPath, newPath) => {
-  const fileName = path.basename(oldPath);
-  const newFilePath = path.join(newPath, fileName);
-  fs.access(oldPath, (err) => {
-    if (err) {
-      console.log("File not found");
-    } else {
-      fs.rename(oldPath, newFilePath, (err) => {
-        if (err) {
-          console.log(err);
-          console.error("FS operation failed");
-        } else {
-          console.log(`File moved to ${newFilePath}`);
-        }
-      });
-    }
+  const readable = fs.createReadStream(oldPath);
+  const writable = fs.createWriteStream(newPath);
+  readable.pipe(writable);
+  readable.on("error", (err) => {
+    console.error("Error reading the file:", err);
+  });
+
+  writable.on("error", (err) => {
+    console.error("Error writing the file:", err);
+  });
+  writable.on("finish", () => {
+    console.log(`File successfully move to ${newPath}`);
+
+    fs.unlink(oldPath, (err) => {
+      if (err) {
+        console.error(`Error deleting the file ${oldPath}:`, err);
+      } else {
+        console.log(`File ${oldPath} successfully deleted.`);
+      }
+    });
   });
 };
