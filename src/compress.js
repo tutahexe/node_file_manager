9e0d297c910e881458a07a30700654377bc77291
Mike <againstthesilence@gmail.com>
2024-10-14 19:28:36 +0300

    Fixed compress/decompress modules

--- src/compress.js
+++ src/compress.js
@@ -1,21 +1,18 @@
-import { createGzip } from "node:zlib";
+import { createBrotliCompress } from "node:zlib";
 import { pipeline } from "node:stream";
 import { createReadStream, createWriteStream } from "node:fs";
 import path from "node:path";
 
 export const compressFile = async (oldPath, newPath) => {
-  const fileName = path.basename(oldPath) + ".gz";
-  const newFilePath = path.join(newPath, fileName);
-  console.log(newFilePath);
-  const gzip = createGzip();
+  const brotli = createBrotliCompress();
   const source = createReadStream(oldPath);
-  const destination = createWriteStream(newFilePath);
-  pipeline(source, gzip, destination, (err) => {
+  const destination = createWriteStream(newPath);
+  pipeline(source, brotli, destination, (err) => {
     if (err) {
       console.error("Operation failed:", err);
       process.exitCode = 1;
     } else {
-      console.log(`File successfully compressed. Can be found ${newFilePath}`);
+      console.log(`File successfully compressed. Can be found ${newPath}`);
     }
   });
 };
--- src/decompress.js
+++ src/decompress.js
@@ -1,15 +1,12 @@
-import { createGunzip } from "node:zlib";
+import { createBrotliDecompress } from "node:zlib";
 import { pipeline } from "node:stream";
 import { createReadStream, createWriteStream } from "node:fs";
-import path from "node:path";
 
 export const decompressFile = async (oldPath, newPath) => {
-  const resultFile = path.basename(oldPath).slice(0, -3);
-  const destinFilePath = path.join(newPath, resultFile);
-  const unzip = createGunzip();
+  const brotliD = createBrotliDecompress();
   const source = createReadStream(oldPath);
-  const destination = createWriteStream(destinFilePath);
-  pipeline(source, unzip, destination, (err) => {
+  const destination = createWriteStream(newPath);
+  pipeline(source, brotliD, destination, (err) => {
     if (err) {
       console.error("Operation failed:", err);
       process.exitCode = 1;
