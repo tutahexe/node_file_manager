cdccc43b248671026997bd404b1adabd0f4ec3b3
Mike <againstthesilence@gmail.com>
2024-10-14 23:46:18 +0300

    reworked copy with streams

--- src/copy.js
+++ src/copy.js
@@ -2,22 +2,17 @@ import fs from "node:fs";
 import path from "node:path";
 
 export const copyFile = async (oldPath, newPath) => {
-  const fileName = path.basename(oldPath);
-  const newFilePath = path.join(newPath, fileName);
-  fs.access(oldPath, (err) => {
-    if (err) {
-      console.error("Old file not found");
-    } else {
-      fs.copyFile(oldPath, newFilePath, (err) => {
-        console.log(oldPath);
-        console.log(newFilePath);
-        if (err) {
-          console.error(err);
-          console.error("FS operation failed");
-        } else {
-          console.log("Directory and files copied");
-        }
-      });
-    }
+  const readable = fs.createReadStream(oldPath);
+  const writable = fs.createWriteStream(newPath);
+  readable.pipe(writable);
+  readable.on("error", (err) => {
+    console.error("Error reading the file:", err);
+  });
+
+  writable.on("error", (err) => {
+    console.error("Error writing the file:", err);
+  });
+  writable.on("finish", () => {
+    console.log(`File successfully copied to ${newPath}`);
   });
 };
